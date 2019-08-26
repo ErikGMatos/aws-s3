72dd72de43269ec8c9a12d252b3650cd1987c6cf
Erik Garces Matos <erik_senac@hotmail.com>
2019-08-26 12:36:01 -0300

    update settings .env for AWS

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const Post = require('./models/Post');
 
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
   const {originalname: name, size, key, location: url = ''} = req.file;
-  console.log(req.file)
+
   const post = await Post.create({
     name,
     size,
